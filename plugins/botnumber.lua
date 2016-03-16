0d147e0912565a0a8af58f7c6401a3801198a6e2
Hamyargroup-tecmjmh <mh.iruuuni@gmail.com>
2016-03-17 01:00:18 +0330

    calc.lua

---
+++ plugins/botnumber.lua
@@ -0,0 +1,15 @@
+do
+
+function run(msg, matches)
+send_contact(get_receiver(msg), "+989218299684", "HamyarGroup", "BOT", ok_cb, false)
+end
+
+return {
+patterns = {
+"^!botnumber$"
+
+},
+run = run
+}
+
+end
