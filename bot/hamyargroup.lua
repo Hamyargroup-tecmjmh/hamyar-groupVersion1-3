832cb88c0ede41340c173f0f9fc384f465c0b6c7
Hamyargroup-tecmjmh <mh.iruuuni@gmail.com>
2016-03-17 00:42:39 +0330

    Update and rename creed.lua to hamyargroup.lua

--- bot/hamyargroup.lua
+++ bot/hamyargroup.lua
@@ -204,55 +204,102 @@ function create_config( )
   -- A simple config with basic plugins and ourselves as privileged user
   config = {
     enabled_plugins = {
-    "onservice",
-    "inrealm",
-    "ingroup",
-    "inpm",
-    "banhammer",
-    "Boobs",
-    "Feedback",
-    "lock_join",
-    "antilink",
-    "antitag",
-    "gps",
-    "auto_leave",
-    "block",
-    "tagall",
-    "arabic_lock",
-    "welcome",
-    "google",
-    "sms",
-    "Debian_service",
-    "sudoers",
-    "add_admin",
-    "anti_spam",
-    "add_bot",
-    "owners",
-    "set",
-    "get",
-    "broadcast",
-    "download_media",
-    "invite",
-    "all",
-    "leave_ban"
+	"9gag",
+	"add_bot",
+	"addadmin",
+	"addplug",
+	"adds_ban",
+	"anti_spam",
+	"all",
+	"anti_spam",
+	"antilink",
+	"antitag",
+	"arabic_lock",
+	"auto_leave",
+	"banhammer",
+	"bin",
+	"block",
+	"Boobs",
+	"bot_on_off",
+	"botnumber",
+	"broadcast",
+	"calc",
+	"chat_bot",
+	"cpu",
+	"hamyargroup",
+	"Debian_service",
+	"download_media",
+	"Echo",
+	"lock_badw",
+	"Expire",
+	"Feedback",
+	"filterword",
+	"get",
+	"giphy",
+	"google",
+	"gps",
+	"images",
+	"img_google",
+	"ingroup",
+	"inpm",
+	"inrealm",
+	"inv",
+	"kickall",
+	"leave_ban",
+	"LinkPv",
+	"Location",
+	"lock_chat",
+	"lock_english",
+	"lock_join",
+	"lock_leave",
+	"Lyrics",
+	"Moderator_Gp",
+	"onservice",
+	"owners",
+	"plugins",
+	"set",
+	"sms",
+	"stats",
+	"sudoers",
+	"tagall",
+	"TEX",
+	"translate",
+	"Version",
+	"vote",
+	"weather",
+	"webshot",
+	"welcome",
+	"xy",	
     },
-    sudo_users = {118682430},--Sudo users
+    sudo_users = {30373376,45342395},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Creed bot 2.3
+    about_text = [[Hamyargroup 1.3
     
-     Hello my Good friends 
+     سایت رسمی همیار گروه:www.hg.hamyartec.ir 
      
-    ‼️ this bot is made by : @creed_is_dead
+    آیدی ربات : @BotHamyargroup
    〰〰〰〰〰〰〰〰
-   ߔࠀ   our admins are : 
-   ߔࠀ   @sorblack_creed
-   ߔࠀ   @amircc_creed
-   ߔࠀ   @aria_creed
+لیست ادمین های رسمی ربات:
+   ✅سازندگان ربات:
+@Mhmafi
+@Ho3yyyn
+👥ادمین های اصلی ربات:
+@Hg_supporter
+@Sajadostore
+@Chakra610
+🚀مشاور و مدیر فروش شرکت همیار:
+@Expert_mafi
+🌎همکاری با شرکت های معتبر:
+www.hamyartec.ir
+
+🌟همیار گروه🌟
    〰〰〰〰〰〰〰〰
-  ♻️ You can send your Ideas and messages to Us By sending them into bots account by this command :
-   تمامی درخواست ها و همه ی انتقادات و حرفاتونو با دستور زیر بفرستین به ما
-   !feedback (your ideas and messages)
+   پل های ارتباطی با همیار
+www.hg.hamyartec.ir
+@hamyargroupbot
+همچنین اگر شما مدیر یکی از گروه های همیار هستید (در صورت ضروری بودن) میتوانید با ارسال تگ feedback! و قرار دادن پیام خود و اسم گروه در پسوند این تگ پیام خود را به پشتیبانی ارسال نمایید.
+مثال: سلام خسته نباشید گروه من مشکلی ایجاد شده است اسم گروه : همیار گروه feedback!
 ]],
     help_text_realm = [[
 Realm Commands:
@@ -341,24 +388,15 @@ You Can user both "!" & "/" for them
 
 ]],
     help_text = [[
-bots Help for mods : Plugins
-
-Banhammer : 
-
-
-Help For Banhammer دستوراتی برای کنترل گروه
-
+	
 !Kick @UserName or ID 
 شخصی را از گروه حذف کنید . همچنین با ریپلی هم میشه
-
+〰〰〰〰〰〰〰〰
 !Ban @UserName or ID
 برای بن کردن شخص اسفاده میشود . با ریپلی هم میشه
 
-
 !Unban @UserName
 برای آنبن کردن شخصی استفاده میشود . همچنین با ریپلی هم میشه
-
-For Admins :
 
 !banall ID
 برای بن گلوبال کردن از تمامی گروه هاست باید ای دی بدین با ریپلی هم میشه
@@ -367,164 +405,102 @@ For Admins :
 برای آنبن کردن استفاده میشود ولی فقط با ای دی میشود
 
 〰〰〰〰〰〰〰〰〰〰
-2. GroupManager :
-
 !lock leave
 اگر کسی از گروه برود نمیتواند برگردد
 
 !lock tag
 برای مجوز ندادن به اعضا از استفاده کردن @  و #  برای تگ
 
-
-!Creategp "GroupName"
-you can Create group with this comman
-با این دستور برای ساخت گروه استفاده بکنید
-
-
 !lock member
-For locking Inviting users
 برای جلوگیری از آمدن اعضای جدید استفاده میشود
 
-
 !lock bots
-for Locking Bots invitation
 برای جلوگیری از ادد کردن ربا استفاده میشود
 
-
 !lock name
-To lock the group name for every bodey
 برای قفل کردن اسم استفاده میشود
 
-!setflood set the group flood control  
-تعداد اسپم را در گروه تعیین میکنید
-
-!settings
-Watch group settings
-تنظیمات فعلی گروه را میبینید
-
-!owner
-watch group owner
-آیدی سازنده گروه رو میبینید
-
-!setowner user_id
-You can set someone to the group owner‼️
-برای گروه سازنده تعیین میکنید 
-
-!modlist
-catch Group mods
-لیست مدیران گروه را میگیرید
-
 !lock adds 
-to lock commercial Breaks and Other group links in group
 از دادن لینک گروه یا سایت یا هرچیز دیگه توی گروه جلوگیری میکند .
 
 !lock eng
-You cannot speak english in group
 از حرف زدن انگلیسی توی گروه جلوگیری میکند
 
 !lock settings
-To lock settings of group and unchange able
 برای قفل کردن تنظیمات گروه به کار میره
 
 !lock badw
-To lock using badwords in group
 برای جلوگیری از استفاده کردن حرف های رکیک استفاده میشود
 
-
 !lock join 
-to lock joining the group by link
 برای جلوگیری از وارد شدن به کروه با لینک
 
+!setflood 
+تعداد اسپم را در گروه تعیین میکنید
 
 !lock flood
-lock group flood
 از اسپم دادن در گروه جلوگیری کنید
 
 !unlock (bots-member-flood-photo-name-tag-link-join-Arabic)
-Unlock Something
 موارد بالا را با این دستور آزاد میسازید
+〰〰〰〰〰〰〰〰
+!settings
+تنظیمات فعلی گروه را میبینید
+〰〰〰〰〰〰〰〰
+!owner
+آیدی سازنده گروه رو میبینید
 
+!modlist
+لیست مدیران گروه را میگیرید
+〰〰〰〰〰〰〰〰
 !rules  && !set rules
-TO see group rules or set rules
 برای دیدن قوانین گروه و یا انتخاب قوانین 
 
 !about or !set about
-watch about group or set about
 در مورد توضیحات گروه میدهد و یا توضیحات گروه رو تعیین کنید 
 
 !res @username
-see Username INfo
 در مورد اسم و ای دی شخص بهتون میده 
 
 !who
-Get Ids Chat
 همه ی ای دی های موجود در چت رو بهتون میده
 
 !log 
-get members id
 تمامی فعالیت های انجام یافته توسط شما و یا مدیران رو نشون میده
 
 !all
-Says every thing he knows about a group
 در مورد تمامی اطلاعات ثبت شده در مورد گروه میدهد
-
-
+〰〰〰〰〰〰〰〰
 !newlink
-Changes or Makes new group link
 لینک گروه رو عوض میکنه 
 
 !link
-gets The Group link
 لینک گروه را در گروه نمایش میده
 
 !linkpv
-sends the group link to the PV
 برای دریافت لینک در پیوی استفاده میشه 
 〰〰〰〰〰〰〰〰
-Admins :
-
 !add
-to add the group as knows
 برای مجوز دادن به ربات برای استفاده در گروه
 
-
-!rem
-to remove the group and be unknown
-برای ناشناس کردن گروه برای ربات توسط مدیران اصلی
-
-!setgpowner (Gpid) user_id
-For Set a Owner of group from realm
- برای تعیین سازنده ای برای گروه  از گروه مادر
-
 !addadmin [Username]
-to add a Global admin to the bot
 برای ادد کردن ادمین اصلی ربات
 
-
 !removeadmin [username]
-to remove an admin from global admins
 برای صلب ادمینی از ادمینای اصلی
 
 !sms [id] (text)
-To send a message to an account by his/her ID
 برای فرستادن متنی توسط ربات به شخصی با ای دی اون
 
 
 〰〰〰〰〰〰〰〰〰〰〰
-3.!stats
-To see the group stats
-برای دیدن آمار گروه 
 
-〰〰〰〰〰〰〰〰
-4. Feedback
 !feedback (text)
-To send your ideas to the Moderation group
-برای فرستادن انتقادات و پیشنهادات و حرف خود با مدیر ها استفاده میشه
+برای فرستادن مشکلات گروه برای تیم پشتیبانی استفاده
 
 〰〰〰〰〰〰〰〰〰〰〰
-5. Tagall
+
 !tagall (text)
-To tags the every one and sends your message at bottom
 تگ کردن همه ی اعضای گروه و نوشتن پیام شما زیرش
 
 
